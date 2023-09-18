0c553837c19fda95821e6e7d40147d6992fe9e3e
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    MotoActions: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- MotoActions/src/org/lineageos/settings/device/ModelNumberActivity.java
+++ MotoActions/src/org/lineageos/settings/device/ModelNumberActivity.java
@@ -20,7 +20,6 @@ package org.lineageos.settings.device;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ModelNumberActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,8 @@ public class ModelNumberActivity extends CollapsingToolbarBaseActivity {
         super.onCreate(savedInstanceState);
         getFragmentManager()
                 .beginTransaction()
-                .replace(R.id.content_frame, new ModelNumberFragment())
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                        new ModelNumberFragment())
                 .commit();
     }
 }
